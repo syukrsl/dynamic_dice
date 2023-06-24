d79e812914f29b29c23ebf2e4f51397c9b0ffc56
syukrsl <mdsyukrsl@gmail.com>
2023-06-24 11:52:34 +0800

    Adds dice functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("All is good");
+// create function when button clicked to roll dice
+
+function rollDice() {
+    // step1: get number from input (n)
+    let number = document.getElementById("dice-number").value;
+    
+    //step2: generate random whole number from 1 to n
+    let result = Math.ceil(Math.random() * number);
+
+    //step3: display results on screen
+    document.getElementById("result").innerHTML = result;
+
+}
